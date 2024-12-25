b42e9b3dbb02e667a823e5fe780de22070bf58a3
Mustafa Abdelati <mustafaabdelati@gmail.com>
2024-12-25 22:12:17 +1100

    created all variables and a basic layout

---
+++ src/lib/components/TimeInput.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { nanoid } from "nanoid/non-secure";
+export const id: string = nanoid();
+export let label:string;
+export let span: string;
+export let value: number;
+</script>
+<div class="text-black flex flex-col gap-1">
+  <label class="text-white" for={id}>{label}</label>
+  <div class="flex gap-1">
+    <input {id} type="time" class="w-24 text-xs rounded rounded-lg outline-none border-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" 
+    bind:value
+    />
+    <span class="text-white">{span}</span>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 // @ts-ignore
 import Input from "$lib/components/Input.svelte";
+import DateInput from "$lib/components/DateInput.svelte";
+import TimeInput from "$lib/components/TimeInput.svelte";
 // 
 let value: number=0;
 let ligandName:string = "ligandName";
@@ -12,9 +14,24 @@ let spreadSheetCell_e17:number= 0.023;
 let spreadSheetCell_c20: number=12;
 let spreadSheetCell_d20: number=24;
 let spreadSheetCell_e20:number= 0.023;
-let spreadSheetCell_c22:number=0.0082
-let spreadSheetCell_c24: number=0.214
-let spreadSheetCell_d24:number = 31.03
+let spreadSheetCell_c22:number=0.0082;
+let spreadSheetCell_c24: number=0.214;
+let spreadSheetCell_d24:number = 31.03;
+let lu_177StockActivity: number = 344;
+let date: Date;
+let volumeOfLigandPrecursor: number=97;
+let volumeOfRadioLabellingBuffer:number=14;
+let volumeOfLu177: number=23;
+let volumeOfRadioLabelledLigand: number=837;
+let volumeOfBindingBuffer: number=523;
+let theoreticalActivity:number=243;
+let totalReactionVolume:number=87;
+let actualActivity:number=23;
+let stockSolutionInBindingBufferActualActivity: any;
+let time:any;
+let percentageOfActivityYield:number = 64;
+let specificActivity: number = 7688;
+let molarActivity: number = 65.23;
 // 
 </script>
 <div class="main bg-gray-900 h-screen w-full flex flex-col">
@@ -31,10 +48,13 @@ let spreadSheetCell_d24:number = 31.03
       <Input bind:value={value} label={"Volume (mL)"}/> 
     </div>
   </div>
+
+  <!-- WRAPPER -->
+  <div class="flex">
   <!-- Planning -->
-  <div class="planning flex flex-col text-white m-8">
-    <h3>Planning</h3>
-    <div class="binding-buffer text-white bg-gray-800 w-1/2 flex flex-col gap-4  p-4 mt-2 text-xs rounded rounded-md">
+    <div class="planning flex flex-col text-white m-8">
+      <h3>Planning</h3>
+      <div class="binding-buffer text-white bg-gray-800  flex flex-col gap-4  p-4 mt-2 text-xs rounded rounded-md">
       <p>Ligand Name <span>{ligandName}</span></p>
       <div class="flex gap-12">
         <div class="flex flex-col gap-2">
@@ -111,16 +131,48 @@ let spreadSheetCell_d24:number = 31.03
           <span>{spreadSheetCell_d24}</span>
         </div>
       </div>
+      </div>
+    </div>
+    <!--  -->
+    <!-- WRAPPER -->
+    <div class="wrapper flex justify-around text-white">
+    <!-- For Radiochemists -->
+    <div class="for-radiochemists" >
+      <h3>For Radiochemists</h3>
+      <div class="binding-buffer text-white bg-gray-800  flex gap-2  p-4 mt-2 text-xs rounded rounded-md">
+        <div class="flex flex-col gap-4">
+          <Input label={"Lu-177 stock activity"} bind:value={lu_177StockActivity} span={"MBq/mL"}/>
+          <DateInput label={"Date"} bind:value={date} span={""}/>
+          <h4 class="text-base">Radio Labelling</h4>
+          <p>Volume of ligand/precursor: <span> {volumeOfLigandPrecursor}</span> μL</p>
+          <p>Volume of Lu-177: <span> {volumeOfLu177}</span> μL</p>
+          <p>Volume of radiolabelling buffer: <span> {volumeOfRadioLabellingBuffer}</span> μL</p>
+          <p>Total reaction volume: <span> {totalReactionVolume}</span> μL</p>
+          <Input label={"Total reaction volume:"} bind:value={actualActivity} span={"MBq"}/>  
+        </div>
+          <div class="flex flex-col gap-4">
+            <h4 class="text-base">Stock solution in binding buffer</h4>
+            <p>Volume of radiolabelled ligand: <span> {volumeOfRadioLabelledLigand}</span> μL</p>
+            <p>Volume of binding buffer: <span> {volumeOfBindingBuffer}</span> ml</p>
+            <p>Theoretical Activity: <span> {theoreticalActivity}</span> μL</p>
+            <Input label={"Actual Activity"} bind:value={stockSolutionInBindingBufferActualActivity} span={"MBq"}/>
+            <TimeInput label={"Time"} bind:value={time} span={"hh:mm"}/>
+          </div>
+      </div>
+    </div>
+  <!-- For Biologists -->
+  <div class="for-biologists">
+    <h3>For Biologists</h3>
+    <div class="binding-buffer text-white bg-gray-800  flex flex-col gap-2  p-4 mt-2 text-xs rounded rounded-md">
+      <p>Percentage of Activity yield: <span> {percentageOfActivityYield}</span> %</p>
+      <p>Specific Activity: <span> {specificActivity}</span> μL</p>
+      <p>Molar Activity: <span> {molarActivity}</span> GBq/µmol</p>
     </div>
   </div>
-  <!--  -->
-  <!-- WRAPPER -->
-  <div class="wrapper flex justify-around">
-    <!-- For Radiochemists -->
-    <div class="for-radiochemists"></div>
-<!-- For Biologists -->
-<div class="for-radiochemists"></div>
+</div>
+</div>
 
-  </div>
+
+
 
 </div>
