e82640e61d5680e9800a1a9b943bd962accae09a
Mustafa Abdelati <mustafaabdelati@gmail.com>
2025-01-30 16:01:11 +1100

    Restyled UI

---
+++ src/lib/components/DisplayInfo.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let label: string;
+	export let measurement: string;
+	export let value: number | string;
+</script>
+
+<div class="flex w-full items-center justify-between">
+	<label>{label}</label>
+	<div class="ml-auto flex items-center gap-1 text-base font-bold">
+		<p>{value}</p>
+		<p class=" ml-auto text-base font-bold"><span> {measurement}</span></p>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import Input from '$lib/components/Input.svelte';
 	import DateInput from '$lib/components/DateInput.svelte';
 	import TimeInput from '$lib/components/TimeInput.svelte';
+	import DisplayInfo from '$lib/components/DisplayInfo.svelte';
 	//
 	let value: number = 0;
 	let ligandName: string;
@@ -35,135 +36,182 @@
 	//
 </script>
 
-<div class="main flex h-auto w-auto flex-col bg-gray-950">
-	<!-- Binding Buffer -->
-	<div class="binding-buffer m-8 flex flex-col rounded rounded-md text-white">
+<!--  -->
+<div class="main flex h-screen w-auto bg-gray-950 text-sm text-white">
+	<!-- nav-bar WRAPPER -->
+	<div class="left-nav-bar relative flex w-1/3 flex-col items-start bg-[#1f2937] p-8">
+		<!-- Binding Buffer -->
 		<h3 class="text-2xl font-bold">Binding Buffer</h3>
-		<div class="inputs mt-2 flex w-[990px] items-end rounded-lg bg-gray-800 p-4">
-			<Input bind:value label={'Total Conc (nM)'} />
-			<span class="m-2">in</span>
-			<Input bind:value label={'Total Activity (MBq)'} />
-			<span class="m-2">of</span>
-			<Input bind:value={ligandName} label={'Ligand Name'} />
-			<span class="m-2">in</span>
-			<Input bind:value label={'Volume (mL)'} />
-		</div>
-	</div>
+		<!-- <div class="relative"> -->
+		<Input bind:value label={'Total Conc'} measurement={'nM'} />
+		<span class=" ml-[85%] font-light">in</span>
+		<!-- </div> -->
+		<Input bind:value label={'Total Activity'} measurement={'MBq'} />
+		<span class="ml-[85%] font-light">of</span>
+		<Input bind:value={ligandName} label={'Ligand Name'} measurement={''} />
+		<span class="ml-[85%] font-light">in</span>
+		<Input bind:value label={'Volume'} measurement={'mL'} />
 
-	<!-- WRAPPER -->
-	<div class="flex items-start">
 		<!-- Planning -->
-		<div class="planning ml-8 flex flex-col text-2xl font-bold text-white">
-			<h3>Planning</h3>
-			<div
-				class="binding-buffer mt-2 flex flex-col gap-4 rounded rounded-md bg-gray-800 p-4 text-xs text-white"
-			>
-				<p>Ligand Name <span>{ligandName}</span></p>
-				<div class="flex flex-col gap-12">
-					<div class="flex flex-col gap-2">
-						<Input bind:value label={'Peptide MW'} span={'μg'} />
-						<Input bind:value label={'Ligand Mass (μg)'} span={'μg'} />
-						<Input bind:value label={'Ligand stock concentration'} span={'mg/mL'} />
-					</div>
-					<div class="flex flex-col gap-2">
-						<Input bind:value label={'Radiolabeling Reaction Volume'} span={'ml'} />
-						<Input bind:value label={'Amount of activity needed for reaction:'} span={'mol'} />
-						<Input bind:value label={'Activity used in radiolabeling'} span={'MBq'} />
-					</div>
-				</div>
-				<div class="mt-8 flex items-center gap-4">
-					<p class="text-lg font-bold">Radiolabelling Reaction Results</p>
-				</div>
-				<!--  -->
-				<div class="flex items-center gap-4">
-					<p>Conc. of Radiolabelled peptide</p>
-					<div class="flex flex-col">
-						<span>M</span>
+		<h3 class="mb-2 mt-4 text-2xl font-bold">Planning</h3>
+		<div class="flex w-full flex-col gap-5">
+			<DisplayInfo label={'Ligand Name'} measurement={''} value={ligandName} />
+			<!-- <p>Ligand Name <span>{ligandName}</span></p> -->
+			<!-- <div class="flex flex-col gap-12"> -->
+			<!-- <div class="flex flex-col gap-2"> -->
+			<Input bind:value label={'Peptide MW'} measurement={'μg'} />
+			<Input bind:value label={'Ligand Mass'} measurement={'μg'} />
+			<Input bind:value label={'Ligand stock concentration'} measurement={'mg/mL'} />
+			<!-- </div> -->
+			<!-- <div class="flex flex-col gap-2"> -->
+			<Input bind:value label={'Radiolabeling Reaction Volume'} measurement={'ml'} />
+			<Input bind:value label={'Amount of activity needed for reaction:'} measurement={'mol'} />
+			<Input bind:value label={'Activity used in radiolabeling'} measurement={'MBq'} />
+		</div>
+		<!-- </div> -->
+		<!-- </div> -->
+		<!--  -->
+	</div>
+	<div class="wrapper flex w-full flex-col items-start justify-between bg-gray-700 p-8 text-white">
+		<!-- Radiolabelling Reaction Results -->
+		<p class=" text-2xl font-bold">Radiolabelling Reaction Results</p>
+		<div class=" flex w-1/2 flex-col items-start rounded-lg border p-4">
+			<!--  -->
+			<div class="flex w-full items-start justify-between">
+				<p>Conc. of Radiolabelled peptide:</p>
+				<div class="flex w-1/3 justify-between">
+					<p class="text-base font-bold">
 						<span>{spreadSheetCell_c20}</span>
-					</div>
-					<div class="flex flex-col">
-						<span>μM</span>
+						<span>M</span>
+					</p>
+					<p class="text-base font-bold">
 						<span>{spreadSheetCell_d20}</span>
-					</div>
-					<div class="flex flex-col">
-						<span>nM</span>
+						<span>μM</span>
+					</p>
+
+					<p class="text-base font-bold">
 						<span>{spreadSheetCell_e20}</span>
-					</div>
+						<span>nM</span>
+					</p>
 				</div>
-				<!--  -->
-				<div class="flex items-center gap-4">
-					<p>Specific Activity​</p>
-					<div class="flex flex-col">
-						<span>​MBq/µg</span>
-						<span>{spreadSheetCell_c22}</span>
-					</div>
-				</div>
-				<!--  -->
-				<div class="flex items-center gap-4">
-					<p>Molar Activity</p>
-					<div class="flex flex-col">
-						<span>MBq/µmol</span>
+			</div>
+			<!--  -->
+			<div class="flex w-full items-start">
+				<p>Specific Activity​:</p>
+				<!-- <div class=" ml-auto flex gap-2 text-base font-bold"> -->
+				<p class="ml-auto text-base font-bold">
+					<span>{spreadSheetCell_c22}</span>
+					<span>​MBq/µg</span>
+				</p>
+				<!-- </div> -->
+			</div>
+			<!--  -->
+			<div class="flex w-full items-start justify-between">
+				<p>Molar Activity:</p>
+				<!-- <div class=" ml-auto flex gap-2 text-base font-bold"> -->
+				<div class="flex w-1/2 justify-between">
+					<p class="ml-auto text-base font-bold">
 						<span>{spreadSheetCell_c24}</span>
-					</div>
-					<div class="flex flex-col">
-						<span>GBq/µmol</span>
+						<span>MBq/µmol</span>
+					</p>
+					<!-- </div> -->
+					<!-- <div class=" ml-auto flex gap-2 text-base font-bold"> -->
+					<p class="ml-auto text-base font-bold">
 						<span>{spreadSheetCell_d24}</span>
-					</div>
+						<span>GBq/µmol</span>
+					</p>
+					<!-- </div> -->
 				</div>
 			</div>
 		</div>
+
 		<!--  -->
-		<!-- WRAPPER -->
-		<div
-			class="wrapper ml-8 flex items-start justify-between gap-8 rounded-lg bg-gray-700 p-8 text-white"
-		>
+		<!--  -->
+
+		<!-- For radiochemists && Biologists -->
+		<div class=" flex w-full justify-between">
 			<!-- For Radiochemists -->
-			<div class="for-radiochemists">
+			<div class="for-radiochemists w-1/2">
 				<h3 class="text-2xl font-bold">For Radiochemists</h3>
 				<div
-					class="binding-buffer mt-2 flex flex-col gap-2 rounded rounded-md bg-gray-800 p-4 text-xs text-white"
+					class="binding-buffer mt-2 flex flex-col rounded rounded-md bg-gray-800 p-4 text-white"
 				>
+					<Input
+						label={'Lu-177 stock activity'}
+						bind:value={lu_177StockActivity}
+						measurement={'MBq/mL'}
+					/>
 					<div class="flex flex-col gap-4">
-						<Input
-							label={'Lu-177 stock activity'}
-							bind:value={lu_177StockActivity}
-							span={'MBq/mL'}
-						/>
-						<DateInput label={'Date'} bind:value={date} span={''} />
-						<div class="flex flex-col gap-4">
-							<h4 class="mt-8 text-lg font-bold">Radio Labelling</h4>
-							<p>Volume of ligand/precursor: <span> {volumeOfLigandPrecursor}</span> μL</p>
-							<p>Volume of Lu-177: <span> {volumeOfLu177}</span> μL</p>
-							<p>
-								Volume of radiolabelling buffer: <span> {volumeOfRadioLabellingBuffer}</span> μL
-							</p>
-							<p>Total reaction volume: <span> {totalReactionVolume}</span> μL</p>
-							<Input label={'Total reaction volume:'} bind:value={actualActivity} span={'MBq'} />
+						<h4 class="mt-4 text-lg font-bold">Radio Labelling</h4>
+						<div class="flex">
+							<p>Volume of ligand/precursor:</p>
+							<p class=" ml-auto text-base font-bold"><span> {volumeOfLigandPrecursor}</span> μL</p>
 						</div>
+						<!--  -->
+
+						<DisplayInfo label={'Volume of Lu-177:'} measurement={'μL'} value={volumeOfLu177} />
+						<!--  -->
+						<DisplayInfo
+							label={'Volume of radiolabelling buffer:'}
+							measurement={'μL'}
+							value={volumeOfRadioLabellingBuffer}
+						/>
+						<!--  -->
+						<DisplayInfo
+							label={'Total reaction volume:'}
+							measurement={'μL'}
+							value={totalReactionVolume}
+						/>
+						<!--  -->
+						<Input label={'Actual Activity:'} bind:value={actualActivity} measurement={'MBq'} />
 					</div>
+					<!--  -->
 					<div class="mt-8 flex flex-col gap-4">
-						<h4 class="text-lg font-bold">Stock solution in binding buffer</h4>
-						<p>Volume of radiolabelled ligand: <span> {volumeOfRadioLabelledLigand}</span> μL</p>
-						<p>Volume of binding buffer: <span> {volumeOfBindingBuffer}</span> ml</p>
-						<p>Theoretical Activity: <span> {theoreticalActivity}</span> μL</p>
+						<h4 class="mt-4 text-lg font-bold">Stock solution in binding buffer</h4>
+						<!--  -->
+						<DisplayInfo
+							label={'Volume of radiolabelled ligand'}
+							measurement={'μL'}
+							value={volumeOfRadioLabelledLigand}
+						/>
+						<!--  -->
+						<DisplayInfo
+							label={'Volume of binding buffer'}
+							measurement={'ml'}
+							value={volumeOfBindingBuffer}
+						/>
+						<!--  -->
+						<DisplayInfo
+							label={'Theoretical Activity'}
+							measurement={'μL'}
+							value={theoreticalActivity}
+						/>
+						<!--  -->
 						<Input
 							label={'Actual Activity'}
 							bind:value={stockSolutionInBindingBufferActualActivity}
-							span={'MBq'}
+							measurement={'MBq'}
 						/>
 						<TimeInput label={'Time'} bind:value={time} span={'hh:mm'} />
+						<DateInput label={'Date'} bind:value={date} span={''} />
 					</div>
 				</div>
 			</div>
 			<!-- For Biologists -->
-			<div class="for-biologists flex flex-col">
+			<div class="for-biologists flex w-1/3 flex-col">
 				<h3 class="text-2xl font-bold">For Biologists</h3>
 				<div
 					class="binding-buffer mt-2 flex flex-col gap-4 rounded rounded-md bg-gray-800 p-4 text-xs text-white"
 				>
-					<p>Percentage of Activity yield: <span> {percentageOfActivityYield}</span> %</p>
-					<p>Specific Activity: <span> {specificActivity}</span> μL</p>
-					<p>Molar Activity: <span> {molarActivity}</span> GBq/µmol</p>
+					<DisplayInfo
+						label={'Percentage of Activity yield'}
+						measurement={'%'}
+						value={percentageOfActivityYield}
+					/>
+					<!--  -->
+					<DisplayInfo label={'Specific Activity'} measurement={'μL'} value={specificActivity} />
+					<!--  -->
+					<DisplayInfo label={'Molar Activity'} measurement={'GBq/µmol'} value={molarActivity} />
 				</div>
 			</div>
 		</div>
